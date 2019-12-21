567ba11391c8142e68d6aafeb3734d2c065bbab5
Daniel <fcdfl1218@163.com>
2019-12-21 16:10:08 +0800

    feat(component): get请求demo
    get请求demo

---
+++ examples/base/app.ts
@@ -0,0 +1,83 @@
+import axios from '../../src/index'
+
+axios({
+    method: 'get',
+    url: '/base/get',
+    params: {
+        foo: ['bar', 'baz']
+    }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+      foo: {
+          bar: 'baz'
+      }
+  }
+})
+
+const date = new Date()
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+      date
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+      foo: '@:$, '
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+      foo: 'bar',
+      baz: null
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get#hash',
+  params: {
+      foo: 'bar'
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get?foo=bar',
+  params: {
+      bar: 'baz'
+  }
+})
+
+// axios({
+//   method: 'post',
+//   url: '/base/post',
+//   data: {
+//       a: 1,
+//       b: 2
+//   }
+// })
+
+// axios({
+//   method: 'get',
+//   url: '/base/post',
+//   headers: {
+//       'content-type': 'application/json;charset=utf-8'
+//   },
+//   data: {
+//       a: 1,
+//       b: 2
+//   }
+// })
